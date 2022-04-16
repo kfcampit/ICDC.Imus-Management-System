13052451c3994ef6a85dd9464cad9b8bd291e86c
kfcampit <75428470+kfcampit@users.noreply.github.com>
2022-04-16 18:25:47 +0800

    Flutter Setup

---
+++ icdc_android_app/android/app/src/main/kotlin/com/example/icdc_android_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.icdc_android_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ icdc_desktop_app/android/app/src/main/kotlin/com/example/icdc_desktop_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.icdc_desktop_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
