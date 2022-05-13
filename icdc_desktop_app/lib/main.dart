4cc18eae86d5a83e7eb5dacaa943c6c57ff6b9f5
kfcampit <75428470+kfcampit@users.noreply.github.com>
2022-05-13 23:27:35 +0800

    Home Page UI

--- icdc_desktop_app/lib/main.dart
+++ icdc_desktop_app/lib/main.dart
@@ -1,115 +1,274 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ICDCDesktop());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class ICDCDesktop extends StatelessWidget {
+  const ICDCDesktop({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
           //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primaryColor: const Color(0xff4b39ef),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        home: Scaffold(
+          body: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [sideBar, homePage],
+          ),
+        ));
   }
 }
+
+Widget sideBar = Container(
+  width: 72,
+  height: 100,
+  child: Padding(
+    padding: const EdgeInsetsDirectional.fromSTEB(0, 32, 0, 32),
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(8, 8, 8, 8),
+            child: Image.asset('assets/logo_1.png')),
+        Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(4, 32, 4, 0),
+            child: IconButton(
+              icon: const Icon(
+                Icons.home_filled,
+                color: Colors.white,
+                size: 32,
+              ),
+              iconSize: 32,
+              onPressed: () {
+                print("Home icon pressed...");
+              },
+            )),
+        Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(4, 4, 4, 4),
+            child: IconButton(
+              icon: const Icon(
+                Icons.person_add,
+                color: Colors.white,
+                size: 32,
+              ),
+              iconSize: 32,
+              onPressed: () {
+                print("Add Patient icon pressed...");
+              },
+            )),
+        Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(4, 4, 4, 4),
+            child: IconButton(
+              icon: const Icon(
+                Icons.search_sharp,
+                color: Colors.white,
+                size: 32,
+              ),
+              iconSize: 32,
+              onPressed: () {
+                print("Search icon pressed...");
+              },
+            )),
+        Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(4, 4, 4, 4),
+            child: IconButton(
+              icon: const Icon(
+                Icons.access_time,
+                color: Colors.white,
+                size: 32,
+              ),
+              iconSize: 32,
+              onPressed: () {
+                print("Recent Patients icon pressed...");
+              },
+            )),
+        Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(4, 4, 4, 4),
+            child: IconButton(
+              icon: const Icon(
+                Icons.inbox_sharp,
+                color: Colors.white,
+                size: 32,
+              ),
+              iconSize: 32,
+              onPressed: () {
+                print("Inventory icon pressed...");
+              },
+            )),
+        Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(4, 4, 4, 4),
+            child: IconButton(
+              icon: const Icon(
+                Icons.access_time,
+                color: Colors.white,
+                size: 32,
+              ),
+              iconSize: 32,
+              onPressed: () {
+                print("Settings icon pressed...");
+              },
+            )),
+        Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(4, 4, 4, 4),
+            child: IconButton(
+              icon: const Icon(
+                Icons.settings,
+                color: Colors.white,
+                size: 32,
+              ),
+              iconSize: 32,
+              onPressed: () {
+                print("Home icon pressed...");
+              },
+            )),
+      ],
+    ),
+  ),
+  decoration: const BoxDecoration(color: Color(0xff4b39ef)),
+);
+
+Widget homePage = Expanded(
+    child: Column(
+  mainAxisAlignment: MainAxisAlignment.center,
+  mainAxisSize: MainAxisSize.max,
+  children: [
+    Image.asset(
+      'assets/logo_large.png',
+      width: 320,
+      height: 320,
+    ),
+    const Padding(
+      padding: EdgeInsetsDirectional.fromSTEB(0, 16, 0, 0),
+      child: Text('Immaculate Concepcion Dental Clinic - Imus',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w700,
+              fontSize: 32,
+              color: Color(0xff4b39ef))),
+    ),
+    const Padding(
+      padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 16),
+      child: Text(
+        'Patient Record and Inventory Management System',
+        style: TextStyle(
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w400,
+            fontSize: 16,
+            color: Color(0xff4b39ef)),
+      ),
+    ),
+    const Divider(
+      thickness: 2,
+      indent: 200,
+      endIndent: 200,
+      color: Color(0xff4b39ef),
+    ),
+    Padding(
+      padding: EdgeInsetsDirectional.fromSTEB(0, 16, 0, 0),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(8, 0, 8, 0),
+            child: Container(
+              height: 48,
+              width: 200,
+              color: const Color(0xff4b39ef),
+              child: TextButton(
+                child: const Text("Add Patient",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20,
+                        color: Colors.white)),
+                onPressed: () {
+                  print("Add Patient button pressed...");
+                },
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(8, 0, 8, 0),
+            child: Container(
+              height: 48,
+              width: 200,
+              color: const Color(0xff4b39ef),
+              child: TextButton(
+                child: const Text("Search Records",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20,
+                        color: Colors.white)),
+                onPressed: () {
+                  print("Search Records button pressed...");
+                },
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(8, 0, 8, 0),
+            child: Container(
+              height: 48,
+              width: 200,
+              color: const Color(0xff4b39ef),
+              child: TextButton(
+                child: const Text("Recent Patients",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20,
+                        color: Colors.white)),
+                onPressed: () {
+                  print("Recent Patients button pressed...");
+                },
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(8, 0, 8, 0),
+            child: Container(
+              height: 48,
+              width: 200,
+              color: const Color(0xff4b39ef),
+              child: TextButton(
+                child: const Text("Check Inventory",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20,
+                        color: Colors.white)),
+                onPressed: () {
+                  print("Check Inventory button pressed...");
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    )
+  ],
+));
